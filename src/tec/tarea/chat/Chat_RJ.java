d68317bde45380e76d2bc9219503f99eaaa0a36c
rey1027 <62966013+rey1027@users.noreply.github.com>
2020-09-25 04:01:54 -0600

    A label, a submit button and its respective event are added. Changes are made to variables. Finally, the option is given that before entering you must put the port you want to use in the connection.

--- src/tec/tarea/chat/Chat_RJ.java
+++ src/tec/tarea/chat/Chat_RJ.java
@@ -1,12 +1,17 @@
 package tec.tarea.chat;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import java.util.Scanner;
 
 /***
  * Class Chat_RJ
@@ -18,8 +23,17 @@ import javafx.stage.Stage;
  * ""----the message is sent with the enter key---""
  */
 
-public class Chat_RJ extends Application {
+public class Chat_RJ extends Application implements EventHandler<ActionEvent> {
     //Attributes
+    /**
+     *
+     */
+    private Scanner read = new Scanner(System.in);
+    /**
+     *
+     */
+    private int port_console = read.nextInt();
+
 
     /**
      * Boolean value to be able to run the client and the server.
@@ -31,6 +45,10 @@ public class Chat_RJ extends Application {
      *
      */
     private boolean isServer = false;
+    /**
+     *
+     */
+    private Label label = new Label("Service Chat RJ");
 
     /***
      * Area where the conversation will be displayed
@@ -41,34 +59,30 @@ public class Chat_RJ extends Application {
      * Make the connection with the abstract class to create the server and the client
      */
     private Connection_SC connection = isServer ? createServer() : createClient();
-
     /**
-     *Display TextField, chat_messages, window measurements.
+     *
+     */
+    private Button button_Send = new Button("SEND");
+    /**
+     *
+     */
+    private TextField input = new TextField();
+    /**
+     *Display window measurements.
      *The event is created where the communication between the server and the client occurs, printing the message in text area.
      * @return root that is specification for the window
      */
     private Parent createContent(){
 
-        chat_messages.setPrefHeight(500);
-        TextField input = new TextField();
-        input.setOnAction( event -> {
-            String message = isServer ? "Server: " : "Client: ";
-            message += input.getText();
-            input.clear();
+        chat_messages.setPrefHeight(375);
+        label.setAlignment( Pos.TOP_CENTER );
+        label.setContentDisplay(ContentDisplay.TOP);
+        label.setFont(new Font("Agency FB",25));
+        button_Send.setFont(new Font("Cambria",25));
+        button_Send.setOnAction(this);
 
-            chat_messages.appendText(message + "\n");
-
-            try{
-                connection.send(message);
-            }
-
-            catch (Exception e){
-                chat_messages.appendText("Failed to send" + "\n");
-            }
-        });
-
-        VBox root = new VBox(20,chat_messages, input);
-        root.setPrefSize(600,600);
+        VBox root = new VBox(20,chat_messages, input,button_Send);
+        root.setPrefSize(450,550);
         return root;
 
     }
@@ -83,6 +97,7 @@ public class Chat_RJ extends Application {
         title_window = isServer ? "Chat RJ Server" : "Chat RJ Client";
         primaryStage.setTitle(title_window);
         primaryStage.setScene(new Scene(createContent()));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     @Override
@@ -95,7 +110,7 @@ public class Chat_RJ extends Application {
      * @return communication port to use
      */
     private Server createServer(){
-        return new Server(55555, data -> Platform.runLater(() -> chat_messages.appendText(data.toString() + "\n")));
+        return new Server(port_console, data -> Platform.runLater(() -> chat_messages.appendText(data.toString() + "\n")));
     }
 
     /**
@@ -103,9 +118,41 @@ public class Chat_RJ extends Application {
      * @return ip address and communication port
      */
     private Client createClient(){
-        return new Client("127.0.0.1",55555, data -> Platform.runLater(() -> chat_messages.appendText(data.toString() + "\n")));
+        return new Client("127.0.0.1",port_console, data -> Platform.runLater(() -> chat_messages.appendText(data.toString() + "\n")));
     }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        System.out.println("NOTA IMPORTANTE: Primero el isServer en true e ingresar el puerto y luego en false realizando el mismo paso"+"***Mismo Puerto***");
+        System.out.println("Luego debe darle enter primero el de true y luego el false para que funcione correctamente");
+        System.out.println("Server =true y Client =false ");
+        System.out.println("\n");
+        System.out.println("Ingrese el puerto que desea utilizar:");
         launch(args);
+    }
+
+    /**
+     *
+     * @param event
+     */
+    @Override
+    public void handle(ActionEvent event) {
+        if ((Button) event.getSource() == button_Send){
+            String message = isServer ? "Server:" : "Client:";
+            message += input.getText();
+            input.clear();
+
+            chat_messages.appendText(message + "\n");
+
+            try{
+                connection.send(message);
+            }
+            catch (Exception e){
+                chat_messages.appendText("Failed to send" + "\n");
+            }
+        }
     }
 }
